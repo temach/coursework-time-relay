f2e18f1d5e8c9ad0a9a7dfaa1f14cf3a08923352
Artem Abramov <tematibr@gmail.com>
2019-05-13 20:46:46 +0300

    tz: add example of how to run aspell for spell checking

--- tz/compile_tech_spec.sh
+++ tz/compile_tech_spec.sh
@@ -2,3 +2,6 @@
 fname=$(basename -s .tex $1)
 pdflatex -synctex=1 ${fname}.tex
 evince ${fname}.pdf &
+
+# to check spelling use the below
+# find . -name "*.tex" -exec aspell --lang=ru --mode=tex check "{}" \;
